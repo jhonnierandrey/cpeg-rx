289db17588ef48234b11b0f212f91d02b70098d9
jhonnierandrey <jhonnierandrey@outlook.com>
2021-10-03 17:31:59 -0300

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 function Footer(){
     return(
-        <footer className="mt-auto">
-            <p>&copy; 2021 | <a href="https://jaesmadeit.com/" target='_blank' rel="noreferrer">JAES Made It</a> | <i>Beta</i></p>
+        <footer id="footer" className="mt-auto">
+            <ul className="footer-copyright">
+                <li>&copy; 2021 <a href="https://www.jaesmadeit.com/" target="_blank" rel="noopener noreferrer">JAES Made It</a></li>
+                <li>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> - <a href="https://github.com/jhonnierandrey/cpeg-rx" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li><a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></li>
+                <li><a href="https://www.buymeacoffee.com/jhonnierandrey" target="_blank" rel="noopener noreferrer"><i className="bi bi-arrow-up-right-circle-fill"></i></a></li>
+            </ul>
         </footer>
     )
 }
