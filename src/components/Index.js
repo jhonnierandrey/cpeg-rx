23e4726b4b5cc3e473e7c4799c6dd74a16bb06c4
jhonnierandrey <jhonnierandrey@outlook.com>
2021-10-26 23:07:08 -0300

    new not found page

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,4 +3,5 @@ export { default as Main } from './Main';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as About } from './About';
+export { default as NotFound } from './NotFound';
 export { default as Footer } from './Footer';
