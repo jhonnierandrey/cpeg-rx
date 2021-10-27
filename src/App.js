996e3ddbd1ab1056f654988ee2a3f57fa34dfae6
jhonnierandrey <jhonnierandrey@outlook.com>
2021-10-26 23:07:20 -0300

    visual improvements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
+import { Header, Main, Login, Register, About, NotFound, Footer } from './components/Index';
+
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <Router>
       <Header />
-      <Main />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
       <Footer />
+      </Router>
     </div>
     );
 }
