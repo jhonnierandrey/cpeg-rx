2e067423a05497ba7c844a0f61bcda4201d1ecd5
jhonnierandrey <jhonnierandrey@outlook.com>
2021-10-26 22:55:07 -0300

    new component about

---
+++ src/components/About.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+
+function About() {
+
+    useEffect(()=>{
+        document.title = 'CPEG | About'
+    })
+
+    return (
+        <main className="px-3">
+          <section className="about">
+            <div className="about-title">
+                <h1>About</h1>
+                <p>
+                    Inspired by this <a href="https://www.reddit.com/r/funny/comments/o5upmq/canceling_plans_excuse_generator_oc/" target='_blank' rel='noreferrer'>Reddit post</a> coded for fun.
+                </p>
+                <p className="lead">
+                    <a
+                    href="https://www.jaesmadeit.com/projects"
+                    className="btn btn-lg btn-outline-primary"
+                    target='_blank'
+                    rel='noreferrer'
+                    >
+                    See more proyects
+                    </a>
+                </p>
+            </div>
+            <div className="about-ava">
+                
+            </div>
+          </section>
+        </main>
+      );
+}
+
+export default About;
